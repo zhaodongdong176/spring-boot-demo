8ac953eeef10d9708f239f1cd6abb9ecc4748327
zhaodongdong176 <zhaodongdong176@163.com>
2019-09-02 11:52:24 +0800

    测试提交controller

---
+++ src/main/java/com/demo/springboot/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.demo.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author zhaodd
+ * @create 2019-09-02-11:48
+ */
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+}

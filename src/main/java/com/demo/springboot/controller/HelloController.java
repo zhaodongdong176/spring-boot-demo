82bfdc810e1c94ba5a8be2b8fd0349fa93a266f7
zhaodongdong <2433349646@qq.com>
2023-04-08 16:12:30 +0800

    删除

--- src/main/java/com/demo/springboot/controller/HelloController.java
+++ src/main/java/com/demo/springboot/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.demo.springboot.controller;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,7 +28,6 @@ public class HelloController {
         System.out.println("2");
         System.out.println("3");
         System.out.println("4");
-        System.out.println("6");
 
         ArrayList<String> s = new ArrayList<>();
 
